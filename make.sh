7da0fd16f5d0eaf29ad90bc276fdd81578ff892a
no-glue <nikola_pav@eunet.rs>
2014-10-19 14:54:14 +0200

    importer unqlite

--- make.sh
+++ make.sh
@@ -1,1 +1,2 @@
 g++ -Wall -W -fpermissive -std=c++11 importer_chained_hash_table.cpp -o importer_chained_hash_table
+g++ -Wall -W -fpermissive -std=c++11 importer_unqlite.cpp -o importer_unqlite -l unqlite
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 cat ../aa-tree/subject_files_test | ./importer_chained_hash_table
+cat ../aa-tree/subject_files_test | ./importer_unqlite
