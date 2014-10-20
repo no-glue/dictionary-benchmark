3815c1ca0d7b972b2149f3bc138fec3ee77c9892
no-glue <nikola_pav@eunet.rs>
2014-10-20 15:45:29 +0200

    aa tree results

--- importer_aa_tree.cpp
+++ importer_aa_tree.cpp
@@ -15,6 +15,7 @@
 #include "generator_file.h"
 #include "importer.h"
 #include "metrics.h"
+#include "timer.h"
 
 using namespace std;
 
@@ -37,7 +38,7 @@ int main() {
     string
   >();
   // tree
-  CstringWrapper * wrapper = new CstringWrapper();
+  CstringWrapper * wrapper = new CstringWrapper(), * str_timer = new CstringWrapper();
   // string helper
   AdapterMetricsAaTree<
     string,
@@ -95,6 +96,16 @@ int main() {
     ifstream
   >();
   // importer
+  Timer<
+    time_t,
+    CstringWrapper,
+    DoubleList<DoubleNode<string>, string>
+  > * timer = new Timer<
+    time_t,
+    CstringWrapper,
+    DoubleList<DoubleNode<string>, string>
+  >(str_timer);
+  // timer
   Metrics<
     // start template list
     AdapterMetricsAaTree<
@@ -126,18 +137,30 @@ int main() {
     >
     // adapter for index (table here)
   >(adapter);
-  time_t now = time(NULL), then;
+  results->insert_right("title", "indexing(seconds)");
+  results->insert_right("title", "nodes");
+  results->insert_right("title", "edges");
+  results->insert_right("title", "density");
+  results->insert_right("title", "average degree");
+  results->insert_right("title", "diameter");
+  results->insert_right("title", "average path length");
+  results->insert_right("title", "bfs(seconds)");
+  results->insert_right("end", "end");
+  timer->set_sooner(time(NULL));
   importer->import(files, tree, file_read);
-  then = time(NULL);
-  cout<<"indexing time "<<difftime(then, now)<<" seconds"<<endl;
+  timer->set_later(time(NULL));
+  timer->set_difference(difftime(timer->get_later(), timer->get_sooner()));
+  timer->collect_difference(results);
   metrics->collect_nodes();
   metrics->collect_edges();
   metrics->collect_density();
   metrics->collect_average_degree();
-  now = time(NULL);
+  timer->set_sooner(time(NULL));
   metrics->breadth_first_search();
-  then = time(NULL);
-  cout<<"bfs time "<<difftime(then, now)<<" seconds"<<endl;
+  timer->set_later(time(NULL));
+  timer->set_difference(difftime(timer->get_later(), timer->get_sooner()));
+  timer->collect_difference(results);
+  results->insert_right("end", "end");
   while(results->get_head()) {
     cout<<results->get_head()->key<<" "<<results->get_head()->value<<endl;
     results->pop_left();
@@ -148,6 +171,8 @@ int main() {
   delete files;
   delete file_read;
   delete importer;
+  delete str_timer;
+  delete timer;
   delete metrics;
   return 0;
 }
