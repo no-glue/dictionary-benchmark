2093b67c9155f30334e28059bfbdc485e5bf304b
no-glue <nikola_pav@eunet.rs>
2014-10-22 02:33:03 +0200

    process status memory mbytes

--- status.h
+++ status.h
@@ -1,20 +1,25 @@
+#if !defined(BUFFER_SIZE)
+#define BUFFER_SIZE 128
+#endif
 template<typename Type, typename Stream, class Wrapper, class List>class Status {
 public:
   Status() {}
   Status(Wrapper * & wrapper):wrapper(wrapper) {}
   void status(List * & results) {
     // pick up process status
-    status(wrapper, key, value, results);
+    status(wrapper, key, value, buffer, results);
   }
 private:
   Wrapper * wrapper;
   char * key;
   char * value;
-  void status(Wrapper * & wrapper, char * key, char * value, List * & results) {
+  char buffer[BUFFER_SIZE];
+  void status(Wrapper * & wrapper, char * key, char * value, char * buffer, List * & results) {
     // pick up process status
     Type line, vmpeak;
     Stream stat_stream("/proc/self/status", Stream::in);
     int i = 0;
+    double temp;
     while(getline(stat_stream, line)) if(!strncmp(line.c_str(), "VmPeak:", 7)) vmpeak = line;
     // any more stats here
     // cat /proc/$$/status for example
@@ -25,6 +30,9 @@ private:
     vmpeak.erase(vmpeak.length() - 2, 2);
     key = wrapper->token(vmpeak.c_str(), ":");
     value = wrapper->token(NULL, ":");
-    results->insert_right(key, value);
+    temp = wrapper->alpha_to_float(value) / 1024.0;
+    wrapper->clear(buffer, BUFFER_SIZE);
+    wrapper->float_to_alpha(buffer, temp);
+    results->insert_right(key, buffer);
   }
 };
