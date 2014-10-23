c2530a6a09cdaa896d3cfacafadece6375972a02
no-glue <nikola_pav@eunet.rs>
2014-10-23 18:58:34 +0200

    graph results sorted

--- graphs_livejournal.sh
+++ graphs_livejournal.sh
@@ -1,1 +1,3 @@
-echo "aa,chained,unqlite VmPeak results*livejournal graph_bars_vmpeak" | python graph_bars.py
+echo "aa,chained,unqlite VmPeak results*livejournal graph_bars_livejournal_vmpeak" | python graph_bars.py
+echo "aa,chained,unqlite indexing(sec) results*livejournal graph_bars_livejournal_indexing_sec" | python graph_bars.py
+echo "aa,chained,unqlite bfs(sec) results*livejournal graph_bars_livejournal_bfs_sec" | python graph_bars.py
--- importer.py
+++ importer.py
@@ -3,13 +3,13 @@ class Importer(object):
     self.glob = glob
   @property
   def glob(self):
-      return self._glob
+    return self._glob
   @glob.setter
   def glob(self, value):
-      self._glob = value
+    self._glob = value
   def files(self, path):
     """Get files to read"""
-    return self.glob.glob(path)
+    return sorted(self.glob.glob(path))
   def file_in_lines(self, file_object):
     """Read file in lines"""
     with file_object as f:
