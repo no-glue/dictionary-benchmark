ee56e6d114d6b0d9697b5011e00f5adc49e0298f
no-glue <nikola_pav@eunet.rs>
2014-10-23 19:28:00 +0200

    livejournal set

---
+++ data/livejournal/get_livejournal.sh
@@ -0,0 +1,8 @@
+wget http://snap.stanford.edu/data/bigdata/communities/com-lj.ungraph.txt.gz
+gzip -d com-lj.ungraph.txt.gz
+mv com-lj.ungraph.txt original
+tail -n +5 original > tmp
+mv tmp original
+split -l 1000000 original livejournal
+find $(pwd) -iname "livejournal" > to_read
+rm original
