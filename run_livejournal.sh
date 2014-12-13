5d841744959fe669dcb17dfdce39ae0c0ccde400
no-glue <nikola_pav@eunet.rs>
2014-12-14 00:44:12 +0100

    running byte table livejournal

--- run_livejournal.sh
+++ run_livejournal.sh
@@ -1,3 +1,4 @@
 ./importer_chained_hash_table < ./subject_files_livejournal
 ./importer_unqlite < ./subject_files_livejournal
 ./importer_aa_tree < ./subject_files_livejournal
+./importer_byte_table < ./subject_files_livejournal
