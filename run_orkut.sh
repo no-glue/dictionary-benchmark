f01f7d8728b7f2da9888d331c498878d3fc73b0d
no-glue <nikola_pav@eunet.rs>
2014-12-14 01:03:33 +0100

    running byte table orkut

--- run_orkut.sh
+++ run_orkut.sh
@@ -1,3 +1,4 @@
 ./importer_chained_hash_table < ./subject_files_orkut
 ./importer_unqlite < ./subject_files_orkut
 ./importer_aa_tree < ./subject_files_orkut
+./importer_byte_table < ./subject_files_orkut
